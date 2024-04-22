72059d1244734d0ba33983b787baf5af24f5aaa0
John Taoi <ctsoi@ucsc.edu>
2024-04-21 18:13:21 -0700

    HTTP Json Respond added

---
+++ app/src/main/java/com/example/proj2/Classes/ChampionViewModel.java
@@ -0,0 +1,4 @@
+package com.example.proj2.Classes;
+
+public class ChampionViewModel {
+}
--- app/src/main/java/com/example/proj2/SecondFragment.java
+++ app/src/main/java/com/example/proj2/SecondFragment.java
@@ -26,13 +26,17 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.json.JSONObject;
 
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
+    private Champion currChamp;
     private String ChampionName;
+    private RequestQueue queue;
 
     @Override
     public View onCreateView(
@@ -49,6 +53,7 @@ public class SecondFragment extends Fragment {
         }else{
             Log.d("Failed","NULL");
         }
+        createChampionObject();
 
         return binding.getRoot();
 
@@ -60,7 +65,7 @@ public class SecondFragment extends Fragment {
         //Change ChampionName TV
         TextView championNameDisplay = getView().findViewById(R.id.champName);
         championNameDisplay.setText(ChampionName);
-        //createChampion();
+        //Log.d("Testing2",currChamp.getName());
         //Return button
         binding.buttonSecond.setOnClickListener(v ->
                 NavHostFragment.findNavController(SecondFragment.this)
@@ -68,6 +73,47 @@ public class SecondFragment extends Fragment {
         );
 
 
+    }
+    public void createChampionObject(){
+        //Base Template
+        String urlTemplate = "https://raw.communitydragon.org/14.5/game/data/characters/";
+        //Modify string for URL usage
+        String urlInsert = ChampionName.replaceAll("\\s", "").toLowerCase();
+        String actualURL = urlTemplate+urlInsert+"/"+urlInsert+".bin.json";
+        queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, actualURL,
+                response -> {
+                    try {
+                        ObjectMapper objectMapper = new ObjectMapper();
+                        JsonNode rootNode = objectMapper.readTree(response);
+                        String pathName = "Characters/" + ChampionName + "/CharacterRecords/Root";
+                        currChamp = new Champion(
+                                rootNode.path(pathName).path("mCharacterName").asText(),
+                                rootNode.path(pathName).path("baseHP").asDouble(),
+                                rootNode.path(pathName).path("hpPerLevel").asDouble(),
+                                rootNode.path(pathName).path("baseDamage").asDouble(),
+                                rootNode.path(pathName).path("damagePerLevel").asDouble(),
+                                rootNode.path(pathName).path("baseArmor").asDouble(),
+                                rootNode.path(pathName).path("armorPerLevel").asDouble(),
+                                rootNode.path(pathName).path("baseSpellBlock").asDouble(),
+                                rootNode.path(pathName).path("spellBlockPerLevel").asDouble(),
+                                rootNode.path(pathName).path("baseMoveSpeed").asDouble(),
+                                rootNode.path(pathName).path("attackSpeed").asDouble(),
+                                rootNode.path(pathName).path("attackSpeedRatio").asDouble(),
+                                rootNode.path(pathName).path("attackSpeedPerLevel").asDouble()
+                        );
+                        Log.d("Testing",currChamp.getName());
+                        // Handle UI updates or further processing here
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> {
+                    error.printStackTrace();
+                });
+        //Log.d("Testing2",currChamp.getName());
+        queue.add(stringRequest);
+        Log.d("Succeed","Succeed");
     }
     @Override
     public void onDestroyView() {
