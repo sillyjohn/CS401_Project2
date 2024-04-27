587a12dc8047d27c8d1b027b075c906235cf00b9
Jimin15 <sujean07015@gmail.com>
2024-04-26 17:40:49 -0700

    UserAccount javadoc

--- app/src/main/java/com/example/proj2/UserAccount.java
+++ app/src/main/java/com/example/proj2/UserAccount.java
@@ -1,42 +1,70 @@
 package com.example.proj2; // Package declaration indicating the location of the class
 
 /**
- * UserAccount Class
+ * The UserAccount class represents a user's account information.
+ * It stores the user's email ID, password, and Firebase UID (user identification token).
  */
 public class UserAccount {
     private String emailId; // Private field to store the user's email ID
     private String password; // Private field to store the user's password
     private String idToken; // Private field to store the Firebase UID (user identification token)
 
-    // Default constructor
+    /**
+     * Constructs a new UserAccount object with default values.
+     */
     public UserAccount() {}
 
-    // Getter method for retrieving the Firebase UID
+    /**
+     * Retrieves the Firebase UID (user identification token) associated with this user account.
+     *
+     * @return The Firebase UID (user identification token)
+     */
     public String getIdToken() {
         return idToken;
     }
 
-    // Setter method for setting the Firebase UID
+
+    /**
+     * Sets the Firebase UID (user identification token) for this user account.
+     *
+     * @param idToken The Firebase UID (user identification token) to set
+     */
     public void setIdToken(String idToken) {
         this.idToken = idToken;
     }
 
-    // Getter method for retrieving the user's email ID
+    /**
+     * Retrieves the email ID associated with this user account.
+     *
+     * @return The email ID
+     */
     public String getEmailId() {
         return emailId;
     }
 
-    // Setter method for setting the user's email ID
+    /**
+     * Sets the email ID for this user account.
+     *
+     * @param emailId The email ID to set
+     */
     public void setEmailId(String emailId) {
         this.emailId = emailId;
     }
 
-    // Setter method for setting the user's password
+    /**
+     * Sets the password for this user account.
+     *
+     * @param password The password to set
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
-    // Getter method for retrieving the user's password
+    /**
+     * Retrieves the password associated with this user account.
+     *
+     * @return The password
+     */
     public String getPassword() {
         return password;
     }
